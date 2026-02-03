1c14f5f5bad700da86da5cd2896d7c56427914f7
Jair Flores <jair.flores.gamarra@gmail.com>
2026-02-03 10:30:57 -0500

    Feature: Login and Register with Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :trackable
+
+  validates :username, presence: true, 
+                       uniqueness: { case_sensitive: false },
+                       length: { minimum: 3, maximum: 20 },
+                       format: { with: /\A[a-zA-Z0-9_]+\z/, message: "only allows letters, numbers and underscores" }
+
+  attr_writer :login
+
+  def login = @login || username || email
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).find_by(
+        "lower(username) = :value OR lower(email) = :value",  value: login.downcase
+      )
+    else
+      find_by(conditions.to_h)
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_18_154648) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_03_144307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,8 +31,10 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_18_154648) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 end
